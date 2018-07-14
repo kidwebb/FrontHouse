9eed629085911762492916a561f06d23fd703e7b
BernardinD <deziusb@knights.ucf.edu>
2018-07-13 21:41:29 -0400

    Testing sending notification payload

--- App/frontHouse/app/src/main/java/com/example/mohamedaitbella/fronthouse/MainActivity.java
+++ App/frontHouse/app/src/main/java/com/example/mohamedaitbella/fronthouse/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mohamedaitbella.fronthouse;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.app.AlertDialog;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONObject;
@@ -23,7 +28,9 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     EditText userName;
     EditText password;
-    Button button;
+    Button button, sending;
+
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +39,22 @@ public class MainActivity extends AppCompatActivity {
         userName=(EditText)findViewById(R.id.userName);
         password=(EditText)findViewById(R.id.password);
         button=(Button)findViewById(R.id.button);
+        sending = (Button)findViewById(R.id.send);
 
         FirebaseMessaging.getInstance().subscribeToTopic("Official");
+
+        final Task task =
+                (FirebaseInstanceId.getInstance().getInstanceId());
+
+        final Activity main = this;
+        task.addOnSuccessListener(main, new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                InstanceIdResult result = (InstanceIdResult)task.getResult();
+                token = result.getToken();
+                Log.d("GET_ID", "" +token );
+            }
+        });
 
     }
 
@@ -77,6 +98,16 @@ public class MainActivity extends AppCompatActivity {
               AlertDialog dialog = builder.create();
               dialog.show();
          }
+    }
+
+    public void sending(View view){
+
+        Send send = new Send();
+
+        send.execute("https://fcm.googleapis.com/fcm/send", "{\"to\":\"/topics/Testing\",\"notification\": {\"title\": \"This is the title\",\"text\": \"Did you make it?!\",\"click_action\": \"MainActivity\"}}", token );
+
+
+
     }
 }
 
@@ -138,6 +169,65 @@ class APICall extends AsyncTask<String, String, JSONObject> {
         return null;
     }
     protected void onPostExecute(JSONObject result){
+        if(result != null)
+            Log.d("MyDebug", result.toString());
+        super.onPostExecute(result);
+    }
+}
+
+class Send extends AsyncTask<String, String, Boolean>{
+
+    public Send(){
+
+    }
+
+    @Override
+    protected void onPreExecute(){
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(String... params){
+
+
+        String urlString = params[0];   // URL being called
+        String data = params[1];    // Data to post
+        String key = params[2];
+
+        Log.d("INPUT CHECK", "URL:" + urlString + ", post: " + data + "key: " + key );
+
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setDoInput(true);     // Allows input to url site
+            urlConnection.setDoOutput(true);    // Allows output from url site
+            urlConnection.setRequestProperty("Content-Type", "application/json");   //Self explanatory
+            urlConnection.setRequestProperty("Accept", "application/json");         //"               "
+            urlConnection.setRequestProperty("Authorization", key);
+            urlConnection.setRequestMethod("POST");                                 //"               "
+            Log.i("message","hello world");
+
+            //---------------- JSON Post Sequence -------------------------------------------
+            OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+            Log.i("traffic", "here");
+            wr.write(data);
+            wr.flush();
+            //-------------------------------------------------------------------------------
+
+            int HttpResult = urlConnection.getResponseCode();   // Self explanatory
+            if(HttpResult == HttpURLConnection.HTTP_OK)
+                return true;
+            else
+                return false;
+        }catch (Exception e){
+            Log.d("CHECK CALL", e.getMessage());
+        }
+
+        return false;
+
+    }
+
+    protected void onPostExecute(Boolean result){
         if(result != null)
             Log.d("MyDebug", result.toString());
         super.onPostExecute(result);
