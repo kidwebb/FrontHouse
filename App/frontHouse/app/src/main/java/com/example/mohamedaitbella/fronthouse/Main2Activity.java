ec3c83451209f6a0050e8df2dea0871b65fb8bd6
BernardinD <deziusb@knights.ucf.edu>
2018-07-16 13:34:35 -0400

    Input Recyclerview framework

--- App/frontHouse/app/src/main/java/com/example/mohamedaitbella/fronthouse/Main2Activity.java
+++ App/frontHouse/app/src/main/java/com/example/mohamedaitbella/fronthouse/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.mohamedaitbella.fronthouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        Adapter adapter = new Adapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
